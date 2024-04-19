aca3cc1a0c92ccfbec6682d87eb2d29c00f209b9
Ana Paula Rabelo <prabelo79@gmail.com>
2024-04-19 11:01:55 -0300

    Atualizar página de inscrição - corrigir validação de campos obrigatórios e adicionar salvamento de usuário no localStorage

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Pegar as cidades do Maranhão e adicionar ao select de Cidade
-var url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/MA/municipios";
+let url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/MA/municipios";
 
-var selectCidades = document.getElementById("cidade");
+let selectCidades = document.getElementById("cidade");
 
 fetch(url)
     .then(response => response.json())
     .then(data => {
         data.forEach(function (cidade) {
-            var option = document.createElement("option");
+            let option = document.createElement("option");
             option.text = cidade.nome;
             selectCidades.add(option);
         });
@@ -16,10 +16,10 @@ fetch(url)
 
 // Mostrar os nome do arquivo anexados e validar a extensão do arquivo
 document.getElementById('documento-res').addEventListener('change', function() {
-    var fileName = this.files[0].name;
-    var fileExtension = fileName.split('.').pop().toLowerCase();
-    var allowedExtensions = ['pdf', 'png', 'jpg'];
-    var fileInfo = document.querySelector('.doc-res__custom-file-input .file-info');
+    let fileName = this.files[0].name;
+    let fileExtension = fileName.split('.').pop().toLowerCase();
+    let allowedExtensions = ['pdf', 'png', 'jpg'];
+    let fileInfo = document.querySelector('.doc-res__custom-file-input .file-info');
     if (allowedExtensions.includes(fileExtension)) {
         fileInfo.querySelector('p').innerText = fileName;
     } else {
@@ -27,10 +27,10 @@ document.getElementById('documento-res').addEventListener('change', function() {
     }
 });
 document.getElementById('documento-id').addEventListener('change', function() {
-    var fileName = this.files[0].name;
-    var fileExtension = fileName.split('.').pop().toLowerCase();
-    var allowedExtensions = ['pdf', 'png', 'jpg'];
-    var fileInfo = document.querySelector('.doc-id__custom-file-input .file-info');
+    let fileName = this.files[0].name;
+    let fileExtension = fileName.split('.').pop().toLowerCase();
+    let allowedExtensions = ['pdf', 'png', 'jpg'];
+    let fileInfo = document.querySelector('.doc-id__custom-file-input .file-info');
     if (allowedExtensions.includes(fileExtension)) {
         fileInfo.querySelector('p').innerText = fileName;
     } else {
@@ -69,17 +69,24 @@ document.getElementById('telefone').addEventListener('input', function(e) {
 });
 
 document.querySelector('.formulario--botoes-enviar').addEventListener('click', function(event) {
-    var documentoId = document.getElementById('documento-id');
-    var documentoRes = document.getElementById('documento-res');
+    let documentoId = document.getElementById('documento-id');
+    let documentoRes = document.getElementById('documento-res');
+    let algumCampoNaoPreenchido = false;
 
-    var camposObrigatorios = document.querySelectorAll('input[required], select[required]');
-    camposObrigatorios.forEach(function (campo) {
+    let camposObrigatorios = document.querySelectorAll('input[required], select[required]');
+    camposObrigatorios.forEach(campo => {
         if (!campo.value){
-            alert('Por favor, preencha todos os campos obrigatórios.');
-            event.preventDefault();
-            return;
+            algumCampoNaoPreenchido = true;
+            document.getElementById(campo.id).style.border = 'var(--vermelho) 1.5px solid';
+            document.querySelector(`label[for="${campo.id}"]`).textContent += ' *';
         }
     });
+
+    if (algumCampoNaoPreenchido) {
+        alert('Por favor, preencha todos os campos.');
+        event.preventDefault();
+        return;
+    }
 
     if (!documentoId.files[0] || !documentoRes.files[0]) {
         alert('Por favor, anexe os dois documentos.');
@@ -88,10 +95,10 @@ document.querySelector('.formulario--botoes-enviar').addEventListener('click', f
     }
 
     // Validar a idade do usuário
-    var dataNascimento = new Date(document.getElementById('data-nascimento').value);
-    var hoje = new Date();
-    var idade = hoje.getFullYear() - dataNascimento.getFullYear();
-    var mes = hoje.getMonth() - dataNascimento.getMonth();
+    let dataNascimento = new Date(document.getElementById('data-nascimento').value);
+    let hoje = new Date();
+    let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+    let mes = hoje.getMonth() - dataNascimento.getMonth();
 
     if (mes < 0 || (mes === 0 && hoje.getDate() < dataNascimento.getDate())) {
         idade--;
@@ -102,6 +109,37 @@ document.querySelector('.formulario--botoes-enviar').addEventListener('click', f
         event.preventDefault();
         return;
     }
+
+    // Salva o usuário no localStorage
+    let usuario = {
+        nome: document.getElementById('nome').value,
+        cpf: document.getElementById('cpf').value,
+        email: document.getElementById('email').value,
+        telefone: document.getElementById('telefone').value,
+        dataNascimento: document.getElementById('data-nascimento').value,
+        cidade: document.getElementById('cidade').value,
+        documentoId: documentoId.files[0].name,
+        documentoRes: documentoRes.files[0].name
+    };
+
+    let localStorageUsuarios = localStorage.getItem('usuario');
+
+    if (localStorageUsuarios) {
+        let usuariosCadastrados = JSON.parse(localStorageUsuarios);
+        if (usuariosCadastrados.cpf === usuario.cpf) {
+            alert('CPF já cadastrado.');
+            event.preventDefault();
+            return;
+        }
+        
+        if (usuariosCadastrados.email === usuario.email) {
+            alert('E-mail já cadastrado.');
+            event.preventDefault();
+            return;
+        }
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
 
     alert('Obrigado pela inscrição! Um link de confirmação foi enviado para o email cadastrado.');
 
